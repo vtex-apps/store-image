ae1034da21bd2660011f6f6ba292b46d0ccc3c46
igorbrasileiro <brasileiro456@gmail.com>
2021-09-10 15:57:51 -0300

    Enhance comment

--- react/Image.tsx
+++ react/Image.tsx
@@ -22,6 +22,11 @@ export interface ImageProps
   experimentalPreventLayoutShift?: boolean
   classes?: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
   preload?: boolean
+  /**
+   * Warning: This property is for internal usage, please avoid use it.
+   * This property is used when the Image is children of SliderTrack component and prevents to trigger the promoView event twice for cloned images.
+   * https://github.com/vtex-apps/slider-layout/blob/master/react/components/SliderTrack.tsx
+   */
   __isDuplicated?: boolean
 }
 
@@ -83,7 +88,6 @@ function Image(props: ImageProps) {
     promotionPosition,
     classes,
     preload,
-    // Do not use this property
     // eslint-disable-next-line
     __isDuplicated,
   } = props
